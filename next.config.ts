e9053658a2a7083a710a3410da8a2050cbb06634
alfonsovillanueva <alfonso@villanuevagarcia.com>
2025-10-10 13:24:15 -0600

    Add variable de entorno al next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "inmuebles-alfgow.s3.mx-central-1.amazonaws.com",
-      },
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "inmuebles-alfgow.s3.mx-central-1.amazonaws.com",
+			},
+		],
+	},
+	env: {
+		INMUEBLES_API_KEY: process.env.INMUEBLES_API_KEY,
+	},
 };
 
 export default nextConfig;
