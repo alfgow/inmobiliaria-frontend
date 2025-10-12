abedb8e2116e71c85f5bba509fe5761753a54b2d
alfgow <64243346+alfgow@users.noreply.github.com>
2025-10-11 23:23:58 -0600

    Switch admin map back to standard OSM tiles

--- src/components/inmuebles/AdminPropertiesMap.tsx
+++ src/components/inmuebles/AdminPropertiesMap.tsx
@@ -236,7 +236,7 @@ const AdminPropertiesMap = ({ properties, isLoading = false }: AdminPropertiesMa
             <MapInstanceBridge onMapReady={setMapInstance} />
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             {markers.map((marker) => (
               <Marker
